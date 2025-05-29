0609fd8a54360f613d37c1e6de77c63cc65284d8
Kaye Louise Manilong <kayelouisemanilong@gmail.com>
2025-05-29 13:26:33 +0800

    Fix: updated necessary files

--- resources/frontend/component/App.jsx
+++ resources/frontend/component/App.jsx
@@ -3,16 +3,20 @@ import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 //import ErrorPage from "./errorElement/ErrorPage";
 //Do not forget to import the pages here
 
+import HomePage from "./Home";
+import Layout from "./Layout";
+
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Layout />,
         //errorElement: <ErrorPage />,
         children: [ 
-            /*{
+            {
                 path: "/",
                 element: <HomePage />,
             },
+            /*
             {
                 path: "/contact-us",
                 element: <Contact />,
